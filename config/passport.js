524ea373992d6cab83f2812080d86d8f38c1212d
Chirag Dhiman <dhimanchirag99@gmail.com>
2026-04-04 11:55:20 +0530

    done

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL:
+        "https://smart-parking-system-backend-oco6.onrender.com/api/auth/google/callback",
       passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, done) => {
