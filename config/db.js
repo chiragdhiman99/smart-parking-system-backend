66efcd478ab4fe33b4e43876a24b44e6051929d1
Chirag Dhiman <dhimanchirag99@gmail.com>
2026-04-06 17:59:36 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        
+        console.log("MongoDB Connected!");
     } catch (error) {
-        
+        console.error("MongoDB Error:", error.message); 
         process.exit(1);
     }
 };
